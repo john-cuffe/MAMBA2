c7f8c0e620ae717842c7b8c510327c9a9d47668d
john-cuffe <37374819+john-cuffe@users.noreply.github.com>
2019-09-10 14:19:57 -0400

    Some extra bits

--- run_Match.bash
+++ run_Match.bash
@@ -1,5 +1,5 @@
 #!/usr/bin/bash
-cd /cygdrive/c/Users/cuffe002/Desktop/Projects/MAMBA2
+cd / ###fill in your mamba directory here
 export data1_name=data1_test  ###First dataset name
 export data2_name=data2_test  ###Second dataset name
 export db_name=mamba_db ###database name
@@ -14,3 +14,7 @@ export rf_jobs=1
 export clerical_review_candidates=True
 export clerical_review_threshold=.3
 export match_threshold=.5
+export chatty_logger=True
+export numWorkers=1
+
+/apps/anaconda/bin/python run_match.py
